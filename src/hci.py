bcab38cf1b3105d94c61746e5f5c786d8679d038
joacomonsalvo <jmonsalvo.business@protonmail.com>
2022-05-09 16:10:47 -0300

    code refactored

--- src/hci.py
+++ src/hci.py
@@ -1,17 +1,13 @@
 from PIL import Image
 from math import sqrt
-import sys
-#
-# TODO LIST
-# get image path
-# finish compression format
-# multiplatform
-#
-def get_pixel_cords_hex(path:str) -> list:
+import argparse
+
+
+def get_pixel_cords_hex(path: str) -> list:
     img = Image.open(path)
     pixels = img.load()
     width, height = img.size
-    data = []
+    codes = []
     #
     for y in range(height):
         for x in range(width):
@@ -19,21 +15,28 @@ def get_pixel_cords_hex(path:str) -> list:
             #
             # alpha channel
             # r, g, b, a = pixels[x, y]
-            data.append([x, y, "{:X}{:X}{:X}".format(r, g, b)])
-    return data
-#
-def sqrt_data(data:list) -> list:
+            codes.append([x, y, "{:X}{:X}{:X}".format(r, g, b)])
+    return codes
+
+
+def sqrt_data(codes: list) -> list:
     data_arr = []
-    for arr in range(len(data)):
-        data_arr.append([int(sqrt(int(data[arr][0]))), int(sqrt(int(data[arr][1]))), '0x'.join(data[arr][2])])
+    for arr in range(len(codes)):
+        sqrt_x = int(sqrt(int(codes[arr][0])))
+        sqrt_y = int(sqrt(int(codes[arr][1])))
+        bin_join = '0x'.join(codes[arr][2])
+        data_arr.append([sqrt_x, sqrt_y, bin_join])
     return data_arr
-#
-def order_data(data:list):
-    sqrt_x = [bin(i[0]) for i in data]
-    sqrt_y = [bin(j[1]) for j in data]
-    bin_hex = [bin(hex(int(k[2], 16))) for k in data]
-    return sqrt_x, sqrt_y, bin_hex
-#
+
+
+def order_data(codes: list) -> list:
+    sqrt_x = [bin(x[0]) for x in codes]
+    sqrt_y = [bin(y[1]) for y in codes]
+    bin_hex = [bin(b[2]) for b in codes]
+    ordered = [sqrt_x, sqrt_y, bin_hex]
+    return ordered
+
+
 if __name__ == "__main__":
     data = sqrt_data(get_pixel_cords_hex("images/sample1.jpg"))
     order_data(data)
